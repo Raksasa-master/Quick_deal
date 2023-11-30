db555870bdf00af22b868dd4e0f0c6614eb6cc76
Raksasa-master <1272940900@qq.com>
2023-11-30 16:26:56 +0800

    0.0.6（更新modinfo内容以及mod图片）

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,18 @@
 name = "Quick_deal"
 author = "Raksasa_master"
 description = [[
-test
+请尽量不要本mod和其他快速交易的mod同时使用
+
+本mod可以与猪王、鸟笼、蚁狮、鱼人王和非自然传送门快速交易
+
+本mod以兼容性为主，所以可以尝试和其他对交易内容有改动的mod一起使用，不过尝试前请记得保存存档，以免发生意外情况炸档
+
+同时保留了原来的交易操作，和原版一样，可以将交易物品直接给予交易目标
+
+快速交易方法为左键“空手”打开交易目标，将交易物品放入箱子中，再点击按键进行交易
+
+兼容永不妥协的怪物蛋交易
+
 ]]
 version = "1.0.0"
 dst_compatible = true
@@ -10,24 +21,10 @@ gorge_compatible = false
 dont_starve_compatible = false
 client_only_mod = false
 all_clients_require_mod = true
---icon_atlas = "modicon.xml"
---icon = "modicon.tex"
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
 forumthread = ""
 api_version_dst = 10
 priority = 0
 mod_dependencies = {}
 server_filter_tags = {}
--- configuration_options = {
--- {
---     --GetModConfigData("")
---     name = "",
---     label = "",
---     hover = "",
---     options = {
---         { data = "", description = "" },
-
---     },
---     default = ""
--- },
-
--- }
